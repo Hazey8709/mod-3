fd562956e00c0f69976d3ce0ad85ddf2afed6535
Hazey8709 <jfortin802@gmail.com>
2023-03-19 18:08:22 -0400

    ud

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const Book = require("../models/books");
 
 //! GET-ALL
 //*  localhost:4000/
@@ -23,9 +25,39 @@ router.get("/:id", (req, res, next) => {
 //! POST
 //*  localhost:4000/:id
 router.post("/", (req, res, next) => {
-    res.json({
-        message: "Books -POST-",
+    const newBook = new Book({
+        id: mongoose.Types.ObjectId(),
+        title: req.body.title,
+        author: req.body.author,
     });
+
+    //! Write DB
+    newBook
+        .save()
+        .then((result) => {
+            console.log(result);
+            res.status(200).json({
+                message: "Book Saved",
+                Book: {
+                    title: result.title,
+                    author: result.author,
+                    id: result.id,
+                    metadata: {
+                        method: req.method,
+                        host: req.hostname,
+                    },
+                },
+            });
+        })
+
+        .catch((err) => {
+            console.error(err.message);
+            res.status(500).json({
+                error: {
+                    message: err.message,
+                },
+            });
+        });
 });
 
 //! PUT
