1bec4132cd59abdd2b30572eac17aa936e9c896b
Hazey8709 <jfortin802@gmail.com>
2023-03-25 21:39:22 -0400

    ud

--- routes/books.js
+++ routes/books.js
@@ -70,34 +70,52 @@ router.get("/:id", (req, res, next) => {
         });
 });
 
-//! POST
-//*  localhost:4000/:id
+//! Post w/check
 router.post("/", (req, res, next) => {
-    const newBook = new Book({
-        _id: mongoose.Types.ObjectId(),
-        title: req.body.title,
-        author: req.body.author,
-    });
-
-    //! Write DB
-    newBook
-        .save()
-        .then((result) => {
-            console.log(result);
-            res.status(200).json({
-                message: "Book Saved",
-                book: {
-                    title: result.title,
-                    author: result.author,
-                    id: result._id,
-                    metadata: {
-                        method: req.method,
-                        host: req.hostname,
+    Book.findOne({ title: req.body.title, author: req.body.author })
+        .then((book) => {
+            if (book) {
+                // Book already exists, return error response
+                return res.status(409).json({
+                    error: {
+                        message: "Book already exists",
                     },
-                },
-            });
-        })
+                });
+            } else {
+                // Book does not exist, create and save new book
+                const newBook = new Book({
+                    _id: mongoose.Types.ObjectId(),
+                    title: req.body.title,
+                    author: req.body.author,
+                });
 
+                newBook
+                    .save()
+                    .then((result) => {
+                        console.log(result);
+                        res.status(200).json({
+                            message: "Book saved",
+                            book: {
+                                title: result.title,
+                                author: result.author,
+                                id: result._id,
+                                metadata: {
+                                    method: req.method,
+                                    host: req.hostname,
+                                },
+                            },
+                        });
+                    })
+                    .catch((err) => {
+                        console.error(err.message);
+                        res.status(500).json({
+                            error: {
+                                message: err.message,
+                            },
+                        });
+                    });
+            }
+        })
         .catch((err) => {
             console.error(err.message);
             res.status(500).json({
@@ -107,6 +125,44 @@ router.post("/", (req, res, next) => {
             });
         });
 });
+
+// //! POST
+// //*  localhost:4000/:id
+// router.post("/", (req, res, next) => {
+//     const newBook = new Book({
+//         _id: mongoose.Types.ObjectId(),
+//         title: req.body.title,
+//         author: req.body.author,
+//     });
+
+//     //! Write DB
+//     newBook
+//         .save()
+//         .then((result) => {
+//             console.log(result);
+//             res.status(200).json({
+//                 message: "Book Saved",
+//                 book: {
+//                     title: result.title,
+//                     author: result.author,
+//                     id: result._id,
+//                     metadata: {
+//                         method: req.method,
+//                         host: req.hostname,
+//                     },
+//                 },
+//             });
+//         })
+
+//         .catch((err) => {
+//             console.error(err.message);
+//             res.status(500).json({
+//                 error: {
+//                     message: err.message,
+//                 },
+//             });
+//         });
+// });
 
 // //! PUT
 // //*  localhost:4000/:bookId
