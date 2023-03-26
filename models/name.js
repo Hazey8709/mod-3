cb695f0ebfec7f7dd7f754723e8958ab0126de57
Hazey8709 <jfortin802@gmail.com>
2023-03-25 21:34:23 -0400

    ud

--- models/name.js
+++ models/name.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 const nameSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    title: String,
-    desc: String,
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
 });
 
 module.exports = mongoose.model("Name", nameSchema);
